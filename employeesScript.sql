8c505095581af3ed1ba1c07f4f67242aa70bdb51
Charlie Dukes <90816184+DukesGuy@users.noreply.github.com>
2021-11-03 21:23:09 -0700

    Update employeesScript.sql

--- employeesScript.sql
+++ employeesScript.sql
@@ -1,7 +1,7 @@
 -- 1. Show all employees who were born before 1965-01-01
 
 SELECT * 
-FROM employees 
+FROM employees e
 WHERE birth_date > '1965-01-01';
 
 -- 2. Show all employees who are female and were hired after 1990
@@ -21,7 +21,7 @@ LIMIT 50;
 
 -- 4. Insert 3 new employees into the employees table. There emp_no should be 100, 101, and 102. You can choose the rest of the data.
 
-INSERT INTO employees 
+INSERT INTO employees e
 VALUES (100, '1993-10-05', 'Charlie', 'Dukes', 'M', '2021-02-27'), 
 (101, '1991-11-20', 'Brittany', 'Dukes', 'F', '2021-02-27'), 
 (102, '1997-12-15', 'Katie', 'Dukes', 'F', '2021-02-27');
@@ -29,27 +29,27 @@ VALUES (100, '1993-10-05', 'Charlie', 'Dukes', 'M', '2021-02-27'),
 
 -- 5. Change the employee's first name to Bob for the employee with the emp_no of 10023.
 
-UPDATE employees 
+UPDATE employees e
 SET first_name = 'Bob' 
 WHERE emp_no = 10023;
 
 
 -- 6. Change all employees' hire dates to 2002-01-01 whose first or last names start with P.
 
-UPDATE employees 
+UPDATE employees e
 SET hire_date = '2002-01-01' 
 WHERE first_name LIKE 'P%' OR last_name LIKE 'P%';
 
 
 -- 7. Delete all employees who have an emp_no less than 10000
 
-DELETE FROM employees 
+DELETE FROM employees e
 WHERE emp_no < 10000;
 
 
 -- 8. Delete all employees who have an emp_no of 10048, 10099, 10234, and 20089.
 
-DELETE FROM employees 
+DELETE FROM employees e
 WHERE emp_no = 10048 OR emp_no = 10099 OR emp_no = 10234 OR emp_no = 20089;
 
 
